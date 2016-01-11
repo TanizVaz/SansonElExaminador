1aeb00bd56409051a0566c6b45be71ca9382ffee
evillasr <villaseduardo@hotmail.com>
2016-01-11 11:17:11 -0600

    Ajuse para que cargue de manera dinamica opciones

--- js/RegistrarBanco.js
+++ js/RegistrarBanco.js
@@ -19,5 +19,12 @@ function cambioRadio(){
       console.log('No select' +selection[i].value);
     }
   }
-  alert(resultado);
+  if(resultado ==='VerFal'){
+    var divCambia = document.getElementById('correcto0');
+    console.log(divCambia.toString());
+    divCambia.innerHTML='<p> Elije la respuesta correcta <input type="radio" name="VF" value="verdadero">Verdadero<input type="radio" name="VF" value="falso">Falso</p>';
+  }else if (resultado ==='OpcionesPer') {
+    var divCambia = document.getElementById('correcto0');
+    divCambia.innerHTML='  <p><input type="text" name="textoPregunta1" value=""><input type="radio" name="VF" value="opc1"><br>        <input type="text" name="textoPregunta2" value=""><input type="radio" name="VF" value="opc2"><br>        <input type="text" name="textoPregunta3" value=""><input type="radio" name="VF" value="opc3"><br>        <input type="text" name="textoPregunta4" value=""><input type="radio" name="VF" value="opc3"><br></p>';
+  }
 }
